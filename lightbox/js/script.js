8c728221e58003d851f6c80fe4632a7be55a2801
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2025-11-05 11:21:46 +0100

    ended lightbox

---
+++ lightbox/js/script.js
@@ -0,0 +1,37 @@
+// DOM variabler
+
+const gallery = document.getElementById("gallery");
+const lightbox = document.getElementById("lightbox");
+const lightBoxImage = document.getElementById("lightboxImage");
+const closeBtn = document.getElementById("closeBtn");
+
+
+function openFullImage(e) {
+
+    // Kontroll att vi klickade på en bild
+    if(e.target.tagName === "IMG") {
+        // Visa lightboxen
+        lightbox.classList.add("active");
+        // Hämta bildadressen för stora bilden
+        let fullImage = e.target.getAttribute("data-full");
+        // Ange bildadressen för stora bilden ill lightbox:en
+        lightBoxImage.setAttribute("src", fullImage);
+    }
+
+}
+
+closeBtn.addEventListener("click", function() {
+    lightbox.classList.remove("active");
+})
+
+lightbox.addEventListener("click", function(e) {
+    if(e.target == lightbox) {
+        lightbox.classList.remove("active");
+    }
+})
+
+
+// Eventlisteners
+gallery.addEventListener("click", openFullImage);
+
+
